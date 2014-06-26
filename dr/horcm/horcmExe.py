8549a56a5a4a7a079f0d5fa318c0c8e988d0e7b9
Jonathan Stoker <fegnoid1@gmail.com>
2014-06-26 11:43:11 +0100

    First itteration of the Horcm executer

---
+++ dr/horcm/horcmExe.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+__author__ = 'Jonathan Stoker'
+
+import subprocess
+
+
+class HorcmExe:
+
+    def __init__(self, horcmnode, horcmcol):
+        self.currentmode = horcmnode
+        self.currentcol = horcmcol
+        self.currentgroups = self.currentcol.collectgroups(self.currentcol.whichhorcm.getter)
+        if (self.currentmode < 0) or (self.currentmode > 1):
+            print "please provide an interger between and 0\n 1 for true DR and 0 for Test"
+
+    def horcmsane(self):
+        groupsane = []
+        for group in self.currentgroups:
+            returncode = subprocess.call("/usr/bin/pairdisplay -ITC"+self.currentcol.whichhorcm.getter+"-g"+group+" -l \
+            | awk '{print $8}' | grep -i pair", shell=True)
+            if returncode == 0:
+                groupsane.append(group)
+        if len(self.currentgroups) == len(groupsane):
+            return True
+        else:
+            return False
+
+    def horcmsaneind(self, group):
+        returncode = subprocess.call("/usr/bin/pairdisplay -ITC"+self.currentcol.whichhorcm.getter+"-g"+group+" -l \
+        | awk '{print $8}' | grep -i pair", shell=True)
+        if returncode == 0:
+            return True
+        else:
+            return False
+
+    def horcmsplit(self):
+        if self.currentmode == 0:
+            print "disks will be split and set RW"
+
+        if self.currentmode == 1:
+            print "Warning this will take over Primary Vols to Secondy site"
+
