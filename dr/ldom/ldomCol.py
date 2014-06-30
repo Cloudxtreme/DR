cdb9158e2043aa7c090945dbe903a1808d8c05ab
Jonathan Stoker <fegnoid1@gmail.com>
2014-06-30 14:19:50 +0100

    Created information collector

--- dr/ldom/ldomCol.py
+++ dr/ldom/ldomCol.py
@@ -1,2 +1,22 @@
 #!/usr/bin/python
 __author__ = 'Jonathan Stoker'
+
+import subprocess
+
+class LdomCol:
+
+    def __init__(self):
+
+
+    def ldomcollector(self):
+        filename = "/var/opt/SUNWldm/ldom-db.xml"
+        ldomfile = open(filename, 'r')
+        ldomarray = []
+        for line in ldomfile:
+            line = line.rstrip()
+            if not line.startswith('<ldom_name>'):
+                continue
+            elif line.startswith('<ldom_name>'):
+                name = line[line.find(">")+1:line.find("<")]
+                ldomarray.append(name)
+        return ldomarray
